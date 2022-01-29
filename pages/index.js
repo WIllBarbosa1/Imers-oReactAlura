4413385ad7c4379321e79ee2a85d570cc10a97fa
Will <dsilverfox98@gmail.com>
2022-01-28 22:28:57 -0300

    Commit inicial

---
+++ pages/index.js
@@ -0,0 +1,170 @@
+import { Box, Button, Text, TextField, Image } from "@skynexui/components"
+import appConfig from "../config.json"
+
+function GlobalStyle() {
+    return (
+        <style global jsx>{`
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+                list-style: nome;
+            }
+
+            body {
+                font-family: 'Open Sans', sans-serif;
+            }
+
+            html, body, #__next {
+                min-height: 100vh;
+                display: flex;
+                flex: 1;
+            }
+
+            #__next {
+                flex: 1;    
+            }
+
+            #__next > * {
+                flex: 1;
+            }
+        `}</style>
+    )
+}
+
+function Titulo(props) {
+
+    const { children, Tag = "h1" } = props
+
+    return (
+        <>
+            <Tag>{children}</Tag>
+
+            <style jsx>{`
+                ${Tag} {
+                    color: ${appConfig.theme.colors.neutrals["000"]};
+                    font-size: 40px;
+                    font-weight: 600;
+                }
+            `}</style>
+        </>
+    )
+}
+
+export default function paginaInicial() {
+    const username = "WillBarbosa1"
+
+    return (
+        <>
+            <GlobalStyle />
+            <Box
+                styleSheet={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: appConfig.theme.colors.primary[500],
+                    backgroundImage: "url(https://thumbs.gfycat.com/QualifiedUncommonAvocet-size_restricted.gif)",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundBlendMode: 'multiply',
+                }}>
+
+                <Box
+                    styleSheet={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        flexDirection: {
+                            xs: 'column',
+                            sm: 'row',
+                        },
+                        width: '100%', maxWidth: '700px',
+                        borderRadius: '5px', padding: '32px', margin: '16px',
+                        boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                        backgroundColor: appConfig.theme.colors.neutrals[700],
+                    }}
+                >
+                    {/* Formulário */}
+                    <Box
+                        as="form"
+                        styleSheet={{
+                            display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                            width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+                        }}
+                    >
+                        <Titulo Tag="h2">Boas vindas de volta!</Titulo>
+                        <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+                            {appConfig.name}
+                        </Text>
+
+                        <TextField
+                            fullWidth
+                            textFieldColors={{
+                                neutral: {
+                                    textColor: appConfig.theme.colors.neutrals[200],
+                                    mainColor: appConfig.theme.colors.neutrals[900],
+                                    mainColorHighlight: appConfig.theme.colors.primary[500],
+                                    backgroundColor: appConfig.theme.colors.neutrals[800],
+                                },
+                            }}
+                        />
+                        <Button
+                            type='submit'
+                            label='Entrar'
+                            fullWidth
+                            buttonColors={{
+                                contrastColor: appConfig.theme.colors.neutrals["000"],
+                                mainColor: appConfig.theme.colors.primary[500],
+                                mainColorLight: appConfig.theme.colors.primary[400],
+                                mainColorStrong: appConfig.theme.colors.primary[600],
+                            }}
+                        />
+                    </Box>
+                    {/* Formulário */}
+
+
+                    {/* Photo Area */}
+                    <Box
+                        styleSheet={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            maxWidth: '200px',
+                            padding: '16px',
+                            backgroundColor: appConfig.theme.colors.neutrals[800],
+                            border: '1px solid',
+                            borderColor: appConfig.theme.colors.neutrals[999],
+                            borderRadius: '10px',
+                            flex: 1,
+                            minHeight: '240px',
+                        }}
+                    >
+                        <Image
+                            styleSheet={{
+                                borderRadius: '50%',
+                                marginBottom: '16px',
+                            }}
+                            src={`https://github.com/${username}.png`}
+                        />
+                        <Text
+                            variant="body4"
+                            styleSheet={{
+                                color: appConfig.theme.colors.neutrals[200],
+                                backgroundColor: appConfig.theme.colors.neutrals[900],
+                                padding: '3px 10px',
+                                borderRadius: '1000px'
+                            }}
+                        >
+                            {username}
+                        </Text>
+                    </Box>
+                    {/* Photo Area */}
+                </Box>
+
+            </Box>
+        </>
+    )
+}
+
+
